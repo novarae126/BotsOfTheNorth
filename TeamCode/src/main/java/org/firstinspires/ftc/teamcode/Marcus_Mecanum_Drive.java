6120828dd87b121ba8d4e6aea0962671c28568bb
ftc21789 <ftc21789@gmail.com>
2026-09-23 14:57:07 -0400

    aded Marcus_Mecanum_Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Marcus_Mecanum_Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Marcus_Mecanum_Drive.java
@@ -1,25 +1,35 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-@TeleOp
-public class Marcus_Mecanum_Drive extends OpMode {
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Marcus_Mecanum_Drive  {
+    OpMode opmode;
+    Telemetry telemetry;
+    HardwareMap hardwaremap;
     private DcMotor frontLeft, frontRight, backLeft, backRight;
     private double x,y,z,max,frontRightCommand,frontLeftCommand, backRightCommand, backLeftCommand;
-    @Override
-    public void init() {
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+
+    public void init(OpMode opMode) {
+        opmode = opMode;
+        telemetry = opmode . telemetry;
+        hardwaremap = opmode . hardwareMap;
+        frontRight = hardwaremap.get(DcMotor.class, "frontRight");
+        frontLeft = hardwaremap.get(DcMotor.class, "frontLeft");
+        backRight = hardwaremap.get(DcMotor.class, "backRight");
+        backLeft = hardwaremap.get(DcMotor.class, "backLeft");
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
-    @Override
+
     public void loop() {
         x=gamepad1.left_stick_x;
         y=gamepad1.left_stick_y;
